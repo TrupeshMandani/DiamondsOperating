f0c09180a6ff7c58bff5994b673ff8d7690611fc
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2025-02-08 21:03:23 -0700

    `feat: updated frontend dependencies and code to include react-router-dom`

--- backend/configurations/db.js
+++ backend/configurations/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectDb = async () => {
+const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
@@ -9,3 +9,4 @@ const connectDb = async () => {
     process.exit(1);
   }
 };
+export default connectDB;
