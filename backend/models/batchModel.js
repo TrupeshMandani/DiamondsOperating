c306cd8944583e9d4e4875e58a3c890b2aee7e6e
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2025-02-21 18:21:15 -0700

    Batch status update backend Login Done.....

--- backend/models/batchModel.js
+++ backend/models/batchModel.js
@@ -5,9 +5,22 @@ const batchSchema = new mongoose.Schema({
   materialType: { type: String, required: true },
   customerName: { type: String, required: true },
   customerContact: { type: String, required: true },
-  currentProcess: { type: String, enum: ["Sarin", "Stitching", "4P Cutting"], required: true },
+  currentProcess: {
+    type: String,
+    enum: ["Sarin", "Stitching", "4P Cutting"],
+    required: true,
+  },
   processStartDate: { type: Date, default: Date.now },
-  status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending" }
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
+  progress: {
+    Sarin: { type: Number, default: 0 },
+    Stitching: { type: Number, default: 0 },
+    "4P Cutting": { type: Number, default: 0 },
+  },
 });
 
 export default mongoose.model("Batch", batchSchema);
