a8f43ed18aadc5323cd0382baf57d12b12fbf37b
rudraa1410 <152832483+rudraa1410@users.noreply.github.com>
2025-03-16 19:58:01 -0600

    Password Updated Feature for Employees from Manager Side

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { login, register ,updatePassword} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.post("/update-password", updatePassword);
+
 
 export default router;
